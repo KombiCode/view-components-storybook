e61b40d3c64f3a5dff8195fe30097c27e2e4205d
Thierry Jet <thierry.jet@kombicode.com>
2021-12-21 18:25:34 +0100

    Add Button view component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/components/**/*.html.erb',
+    './app/components/**/*.rb'
   ]
 }
